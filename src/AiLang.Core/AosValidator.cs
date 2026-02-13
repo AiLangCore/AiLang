b1616b072e62bf2b5b577655cd6e87d8fed52550
Todd Henderson <todd@todd-henderson.me>
2026-02-13 11:14:04 -0600

    feat(http): add structured request body parsing primitives

--- src/AiLang.Core/AosValidator.cs
+++ src/AiLang.Core/AosValidator.cs
@@ -725,6 +725,34 @@ public sealed class AosValidator
             return AosValueKind.Node;
         }
 
+        if (target == "compiler.parseHttpBodyForm")
+        {
+            RequirePermission(node, "compiler", permissions);
+            if (argTypes.Count != 1)
+            {
+                _diagnostics.Add(new AosDiagnostic("VAL150", "compiler.parseHttpBodyForm expects 1 argument.", node.Id, node.Span));
+            }
+            else if (argTypes[0] != AosValueKind.String && argTypes[0] != AosValueKind.Unknown)
+            {
+                _diagnostics.Add(new AosDiagnostic("VAL151", "compiler.parseHttpBodyForm arg must be string.", node.Id, node.Span));
+            }
+            return AosValueKind.Node;
+        }
+
+        if (target == "compiler.parseHttpBodyJson")
+        {
+            RequirePermission(node, "compiler", permissions);
+            if (argTypes.Count != 1)
+            {
+                _diagnostics.Add(new AosDiagnostic("VAL152", "compiler.parseHttpBodyJson expects 1 argument.", node.Id, node.Span));
+            }
+            else if (argTypes[0] != AosValueKind.String && argTypes[0] != AosValueKind.Unknown)
+            {
+                _diagnostics.Add(new AosDiagnostic("VAL153", "compiler.parseHttpBodyJson arg must be string.", node.Id, node.Span));
+            }
+            return AosValueKind.Node;
+        }
+
         if (target == "compiler.route")
         {
             RequirePermission(node, "compiler", permissions);

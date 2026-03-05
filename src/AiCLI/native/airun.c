dce5ac8757c1b59be81d1de31b58df0122adf21d
Todd Henderson <todd@todd-henderson.me>
2026-03-05 01:11:14 -0600

    fix: include POSIX select headers for fullstack server loop

--- src/AiCLI/native/airun.c
+++ src/AiCLI/native/airun.c
@@ -25,8 +25,10 @@
 #include <fcntl.h>
 #include <netinet/in.h>
 #include <signal.h>
+#include <sys/select.h>
 #include <sys/socket.h>
 #include <sys/stat.h>
+#include <sys/time.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <sys/wait.h>
